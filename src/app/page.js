034fdfd637795d526522a0930e8c13dda64df210
Deep <dprajap2@gmail.com>
2024-06-04 17:49:00 -0400

    update page

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,27 @@
 "use client";
 
-import { Box, Heading, } from "@chakra-ui/react";
+import { Box, Heading, IconButton } from "@chakra-ui/react";
+import { socials } from "./_lib/data";
+import { SocialLink } from "./_components/SocialLink";
 
 export default function Home() {
   return (
-  <Box>
-    <Box>
-      This is for socials is fixed
-    </Box>
-
-    <Heading as="h1" size="2xl">This is my name</Heading>
-    <Heading as={'h2'}>This is a description of me</Heading>
-    <Box>
-      <Box>
-        This holds the work and is fixed
+    <Box minH={"100vh"} display={"flex"} flexDir={"column"}>
+      <Box display="flex" justifyContent="center" alignItems="center" gap="1">
+        {socials.map((social) => (
+          <SocialLink social={social} key={social.name} />
+        ))}
       </Box>
-      <Box>This holds the projects and is scrollable</Box>
-      <Box>This holds skills maybe</Box>
+
+      <Heading as="h1" size="2xl">
+        This is my name
+      </Heading>
+      <Heading as={"h2"}>This is a description of me</Heading>
+      <Box>
+        <Box>This holds the work and is fixed</Box>
+        <Box>This holds the projects and is scrollable</Box>
+        <Box>This holds skills maybe</Box>
+      </Box>
     </Box>
-  </Box>
-  )
+  );
 }
